bd4aa06493378da5fd3c84fc69178f8d766d4975
yuki-inui <91231239+yuki-inui@users.noreply.github.com>
2021-10-02 21:43:55 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const fs = require('fs');
 const fileName = './test.txt';
 for (let count = 0; count < 500; count++) {
-  fs.appendFile(fileName, 'あ', 'utf8', () => {});
-  fs.appendFile(fileName, 'い', 'utf8', () => {});
-  fs.appendFile(fileName, 'う', 'utf8', () => {});
-  fs.appendFile(fileName, 'え', 'utf8', () => {});
-  fs.appendFile(fileName, 'お', 'utf8', () => {});
-  fs.appendFile(fileName, '\n', 'utf8', () => {});
+  fs.appendFileSync(fileName, 'あ', 'utf8', () => {});
+  fs.appendFileSync(fileName, 'い', 'utf8', () => {});
+  fs.appendFileSync(fileName, 'う', 'utf8', () => {});
+  fs.appendFileSync(fileName, 'え', 'utf8', () => {});
+  fs.appendFileSync(fileName, 'お', 'utf8', () => {});
+  fs.appendFileSync(fileName, '\n', 'utf8', () => {});
 }
---
+++ promise-test.js
@@ -0,0 +1,21 @@
+'use strict';
+const fs = require('fs');
+const fileName = './test.txt';
+
+function appendFilePromise(fileName, str) {
+  return new Promise((resolve) => {
+    fs.appendFile(fileName, str, 'utf8', () => resolve());
+  });
+}
+async function main() {
+  for (let count = 0; count < 500; count++) {
+    await appendFilePromise(fileName, 'あ');
+    await appendFilePromise(fileName, 'い');
+    await appendFilePromise(fileName, 'う');
+    await appendFilePromise(fileName, 'え');
+    await appendFilePromise(fileName, 'お');
+    await appendFilePromise(fileName, '\n');
+  }
+}
+
+main();
